e00c63b97abfac22f74723127f53ad515ebb6d59
vrana <vibhanshurana10@gmail.com>
2021-03-07 17:59:49 +0530

    Vue JS 3 tutorial in Hindi #2 How to use VueJs with CDN

---
+++ app.js
@@ -0,0 +1,12 @@
+console.warn("Hello from app.js")
+// import Vue from 'vue'
+ 
+// const {createApp} = Vue;
+const app = Vue.createApp({
+    template: "<h1>Hello Vue js</h1>"
+
+})
+app.mount("#app")
+
+
+ 

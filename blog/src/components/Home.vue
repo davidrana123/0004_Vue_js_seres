0dec208ced0459daef539e227e7557c907210649
vrana <vibhanshurana10@gmail.com>
2021-03-11 02:04:12 +0530

    form

--- blog/src/components/Home.vue
+++ blog/src/components/Home.vue
@@ -1,8 +1,11 @@
 <template>
 <h1>Simple Form</h1>
-<p>
-    {{form}}
-</p>
+<ul>
+        <li v-for="item in error" v-bind:key="item">
+            {{item}} not valid
+
+        </li>
+</ul>
 <form>
     <label>Email : </label>
     <input type="text" placeholder="Enter Email" v-model="form.email" />
@@ -12,26 +15,25 @@
     <input type="password" placeholder="Enter Password" v-model="form.password" />
     <br /><br />
     <select v-model="form.country">
-      <option>India</option>
-       <option>China</option>
-        <option>USA</option>  
+        <option>India</option>
+        <option>China</option>
+        <option>USA</option>
     </select>
-     <br /><br />
-    <h3>Technology</h3>
-     <label>Java : </label>
-    <input type="checkbox" value="java"  v-model="form.technology" />
     <br /><br />
-     <label>.Net : </label>
+    <h3>Technology</h3>
+    <label>Java : </label>
+    <input type="checkbox" value="java" v-model="form.technology" />
+    <br /><br />
+    <label>.Net : </label>
     <input type="checkbox" value=".net" v-model="form.technology" />
     <br /><br />
-     <h3>Gender</h3>
-     <label>Male : </label>
-    <input type="radio" value="male" name="gender"  v-model="form.gender" />
+    <h3>Gender</h3>
+    <label>Male : </label>
+    <input type="radio" value="male" name="gender" v-model="form.gender" />
     <br /><br />
-     <label>Female : </label>
+    <label>Female : </label>
     <input type="radio" value="female" name="gender" v-model="form.gender" />
     <br /><br />
-
 
     <button v-on:click="login" type="button">Login</button>
 </form>
@@ -48,12 +50,24 @@ export default {
                 country: '',
                 technology: [],
                 gender: ''
-            }
+            },
+            error: [],
         }
     },
     methods: {
         login() {
-            console.warn("login data", this.form)
+            this.error = [];
+            for (const item in this.form) {
+                if (this.form[item] === "" || this.form[item].length === 0) {
+                    this.error.push(item)
+                }
+            }
+
+            if (this.error.length === 0) {
+                alert("data has been submited");
+            }
+
+            console.warn(this.form, this.error)
         }
     }
 
